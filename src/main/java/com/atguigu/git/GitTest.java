2351da2b3800a40ccf58cef8e70c5826af1b0117
LuLuLin <764613693@qq.com>
2022-10-29 20:03:02 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest  {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git4");
+        System.out.println("hot-fix test");
         System.out.println("hello git5");
     }
 }
