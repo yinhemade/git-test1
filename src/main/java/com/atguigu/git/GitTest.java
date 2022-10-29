56934e0407ec2c50a3b0abeecfdde8688fc17e6f
LuLuLin <764613693@qq.com>
2022-10-29 19:03:31 +0800

    fourth commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,11 +1,13 @@
 
 
-package com.atguigu.git;
+package atguigu.git;
 
 public class GitTest  {
     public static void main(String[] args) {
 
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
